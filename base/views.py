5bcb47f218f99865063861116da9107d974f2b71
SamyCode <sambea2103@gmail.com>
2023-05-10 19:23:08 -0400

    Login and Logout pages and logic

--- base/views.py
+++ base/views.py
@@ -2,6 +2,32 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from .models import Room,Topic
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+def loginPage(request):
+    if request.method =="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request, "User does not exist")
+        user = authenticate(request, username=username, password =password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+           messages.error(request, "Values entered are not correct, user does not exist") 
+        
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 #* Home Page View:
